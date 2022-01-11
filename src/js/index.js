6e22952908ea8b74742b8abeb63fc6f24bf73516
kutuzovpetia <petiakutuzov@gmail.com>
2022-01-11 14:32:19 +0200

    Added: rating logic

--- src/js/index.js
+++ src/js/index.js
@@ -2,3 +2,72 @@ import '../style/style.scss';
 
 
 
+class Rating{
+
+    constructor() {
+        this.ratingStars = [...document.querySelectorAll('.form-rating__item > div')];
+        this.init();
+    }
+
+    executeRating(stars){
+        const starClassActive = "star-full";
+        const starClassInactive = "star-empty";
+        const starsLength = stars.length;
+        let i;
+
+        stars.map((star) => {
+
+            star.onclick = () => {
+                i = stars.indexOf(star);
+                console.log(i+1)
+                if(star.className === starClassInactive)
+                    for(i; i >= 0; --i) stars[i].className = starClassActive;
+                else
+                    for(i; i < starsLength; ++i) stars[i].className = starClassInactive
+            };
+        });
+    }
+
+    init(){
+        this.executeRating(this.ratingStars);
+    }
+}
+
+
+window.addEventListener('DOMContentLoaded', ()=>{
+
+    new Rating();
+
+
+
+
+    // const ratingStars = [...document.querySelectorAll('.form-rating__item > div')];
+    //
+    // function executeRating(stars){
+    //     const starClassActive = "star-full";
+    //     const starClassInactive = "star-empty";
+    //     const starsLength = stars.length;
+    //     let i;
+    //
+    //     stars.map((star) => {
+    //
+    //         star.onclick = () => {
+    //             i = stars.indexOf(star);
+    //             console.log(i+1)
+    //             if(star.className === starClassInactive)
+    //                 for(i; i >= 0; --i) stars[i].className = starClassActive;
+    //             else
+    //                 for(i; i < starsLength; ++i) stars[i].className = starClassInactive
+    //
+    //
+    //
+    //         };
+    //
+    //     });
+    //
+    //
+    // }
+    //
+    // executeRating(ratingStars);
+
+})
