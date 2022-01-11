c7dcafe27ea322df5968d7e3b4345b355471076d
kutuzovpetia <petiakutuzov@gmail.com>
2022-01-11 17:12:45 +0200

    +Logic

--- src/js/index.js
+++ src/js/index.js
@@ -1,11 +1,28 @@
 import '../style/style.scss';
 
 
-
-class Rating{
+class FormLogic{
 
     constructor() {
         this.ratingStars = [...document.querySelectorAll('.form-rating__item > div')];
+        this.checkBox = document.querySelector('.custom-checkbox');
+        this.inputsWrapper = document.querySelector('.form-contact__inputs');
+
+        this.inputContactName = document.getElementById('contactName');
+        this.inputMobileNumber = document.getElementById('mobileNumber');
+        this.inputEmail = document.getElementById('email');
+        this.textArea = document.getElementById('textArea');
+
+        this.buttonSubmit = document.querySelector('.form-button__submit');
+
+        this.submitInfo = {
+            rating: null,
+            feedback: "",
+            contactName: "",
+            mobile: "",
+            email: "",
+        }
+
         this.init();
     }
 
@@ -20,6 +37,7 @@ class Rating{
             star.onclick = () => {
                 i = stars.indexOf(star);
                 console.log(i+1)
+                this.submitInfo.rating = i + 1;
                 if(star.className === starClassInactive)
                     for(i; i >= 0; --i) stars[i].className = starClassActive;
                 else
@@ -28,46 +46,72 @@ class Rating{
         });
     }
 
+    validateEmail(email){
+        return String(email)
+            .toLowerCase()
+            .match(/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
+    };
+
+    checkBoxToggle(checked){
+        checked ? this.inputsWrapper.classList.add('active') : this.inputsWrapper.classList.remove('active')
+    }
+
+    eventCheckBox(){
+        this.checkBox.addEventListener('change',  ()=>{
+            this.checkBoxToggle(this.checkBox.checked)
+        })
+    }
+
+    addListeners(event, elements){
+        elements.forEach(item=>{
+            item.input.addEventListener(event, (e)=>item.cb(e));
+        })
+    }
+
+    eventsInputs(){
+
+        const feedback = (e) => this.submitInfo.feedback = e.target.value;
+        const contactName = (e) => this.submitInfo.contactName = e.target.value;
+        const mobile = (e) => this.submitInfo.mobile = e.target.value;
+        const email = (e) => this.submitInfo.email = e.target.value;
+
+        const elems = [
+            { input: this.inputContactName, cb: contactName },
+            { input: this.inputMobileNumber, cb: mobile },
+            { input: this.textArea, cb: feedback },
+            { input: this.inputEmail, cb: email },
+
+        ]
+
+        this.addListeners('keyup', elems)
+    }
+
+    submit(){
+        this.buttonSubmit.addEventListener('click', (e)=>{
+            e.preventDefault();
+
+
+            if(this.checkBox.checked || !this.validateEmail(this.submitInfo.email) || !this.submitInfo.contactName || !this.submitInfo.mobile){
+                alert('!!!')
+            }
+            else{
+                console.log(this.submitInfo)
+            }
+
+        })
+    }
+
     init(){
         this.executeRating(this.ratingStars);
+        this.eventCheckBox();
+        this.eventsInputs();
+        this.submit();
     }
 }
 
 
 window.addEventListener('DOMContentLoaded', ()=>{
 
-    new Rating();
-
-
-
-
-    // const ratingStars = [...document.querySelectorAll('.form-rating__item > div')];
-    //
-    // function executeRating(stars){
-    //     const starClassActive = "star-full";
-    //     const starClassInactive = "star-empty";
-    //     const starsLength = stars.length;
-    //     let i;
-    //
-    //     stars.map((star) => {
-    //
-    //         star.onclick = () => {
-    //             i = stars.indexOf(star);
-    //             console.log(i+1)
-    //             if(star.className === starClassInactive)
-    //                 for(i; i >= 0; --i) stars[i].className = starClassActive;
-    //             else
-    //                 for(i; i < starsLength; ++i) stars[i].className = starClassInactive
-    //
-    //
-    //
-    //         };
-    //
-    //     });
-    //
-    //
-    // }
-    //
-    // executeRating(ratingStars);
+    new FormLogic();
 
 })
